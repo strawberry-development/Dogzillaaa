8eb247b848b70770b6830c110f7d35af68fa5ed5
Anon <strawberry-development@protonmail.com>
2024-08-08 17:03:14 +0200

    added a logo
    make a small logo and put it as a favicon

--- views/index.php
+++ views/index.php
@@ -20,6 +20,10 @@ $message = isset($_GET['message']) ? htmlspecialchars($_GET['message']) : '';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dogzillaaa</title>
     <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/img/logo/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/logo/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/img/logo/favicon-16x16.png">
+    <link rel="manifest" href="../assets/img/logo/site.webmanifest">
 </head>
 <body>
 <header>
